6d64630e5a3e7027af3663f86d9c327346e9d3d4
dayfundora <dayfundoraglez@gmail.com>
2021-04-05 01:30:06 -0400

    Create Bag.pl

---
+++ Bag.pl
@@ -0,0 +1,32 @@
+%*****************%
+%****** Bag ******%
+%*****************%
+
+%initializeBag / 1
+%initializeBag (Bag)
+%Generates a bag (Bag) for the beginning of the game with 20 tiles of each color.
+initializeBag ([[blue, 20], [yellow, 20], [white, 20], [black, 20], [red, 20]]).
+
+%makeColorsList / 2
+%makeColorsList (Bag, List)
+%From a bag (Bag) with its characteristic structure, generate a list (List) with all the tiles in the bag, it will be a list of colors.
+makeColorsList ([], []): -!
+makeColorsList ([[Color, 0] | BagL], List): - makeColorsList (BagL, List),!.
+makeColorsList ([[Color, CantColor] | BagL], List): - append (List, [Color]), CantColorR is CantColor-1, makeColorsList ([[Color, CantColorR] | BagL], List).
+
+%takeTileRandomBag / 3
+%takeTileRandomBag (Bag, Color, BagR)
+%Select a random (Color) tile from the bag (Bag)
+takeTileRandomBag (Bag, Color, BagR): - makeColorsList (Bag, List), lenght (List, L), random (0, L, I), nth0 (I, List, Color), putNColorBag (Bag, Color, BagR) .
+
+%putNColorBag / 4
+%putNColorBag (Bag, Color, Quantity, BagR)
+%Puts N Color tiles in the bag.
+putNColorBag ([[Color, QtyColor] | BagL], Color, Qty, [[Color, QtyColorR] | BagLR]): - QtyColorR is QtyColor + Qty,!.
+putNColorBag ([[OtherColor, QtyOtherColor] | BagL], Color, Qty, [[OtherColor, QtyOtherColor] | BagLR]): - putNColorBag (BagL, Color, Qty, BagLR).
+
+%putNColorsBag / 3
+%putNColorsBag (Bag, ListColorCant, BagR)
+%Puts a list of colors (with their respective quantities in the bag color tiles Color in the bag.
+putNColorsBag (Bag, [], Bag): - !.
+putNColorsBag (Bag, [[Color, Qty], L], BagR): - putNColorBag (Bag, Color, Qty, BagT), putNColorsBag (BagT, L, BagR).
