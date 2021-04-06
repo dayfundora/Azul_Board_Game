5fbef6100b463f1d82736cc3a18a0d26b01844bb
dayfundora <dayfundoraglez@gmail.com>
2021-04-06 01:34:37 -0400

    Add Bag to Cover

--- Cover.pl
+++ Cover.pl
@@ -1,11 +1,19 @@
 %*****************%
 %******Cover******%
 %*****************%
+:-["Bag.pl"].
 
 %initializeCover / 1
 %initializeCover (Cover)
 %Generates a cover (Cover) for the start of the game with 0 tiles of each color.
 initializeCover ([[blue, 0], [yellow, 0], [white, 0], [black, 0], [red, 0]]).
+
+%initializeCover / 4
+%initializeCover (Bag, Cover, BagR, CoverR, NeedBool)
+%Puts all tiles from the cover (Cover) into the Bag, if needed.
+initializeCover (Bag, Cover, Bag, Cover, 0): - !.
+initializeCover (Bag, [[blue, 0], [yellow, 0], [white, 0], [black, 0], [red, 0]], Bag, [[blue, 0], [yellow, 0] , [white, 0], [black, 0], [red, 0]], 1): - !.
+initializeCover (Bag, [[Color, CantColor] | CoverList], BagR, [[Color, 0] | CoverListR], 1): - putNColorsBag (Bag, Color, CantColor, BagT), initializeCover (BagT, CoverList, BagR, CoverListR ).
 
 %putNColorCover / 4
 %putNColorCover (Cover, Color, Quantity, CoverR)
