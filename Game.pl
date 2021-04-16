ffe26699e991162721233f27646ac14cc806c1ce
dayfundora <dayfundoraglez@gmail.com>
2021-04-16 01:33:21 -0400

    Add round to Game

--- Game.pl
+++ Game.pl
@@ -29,3 +29,20 @@ play(Players,Bag,Factories,Center,Cover,0,PlayersR):-offertFactory(Players,Bag,F
  prepareNextRound(PlayersS,BagT,FactoriesT,CenterT,CoverS,PlayersM,BagR,FactoriesR,CenterR,CoverR), 
  print('  The round is over ')
  play(PlayersM,BagR,FactoriesR,CenterR,CoverR,IsOver,PlayersR).
+
+%offertFactory/10
+%offertFactory(Players,Bag,Factories,Center,Cover,PlayersR,BagR,FactoriesR,CenterR,CoverR)
+%Wrapper for round
+offertFactory(Players,Bag,Factories,Center,Cover,PlayersR,BagR,FactoriesR,CenterR,CoverR):-round(Players,Bag,Factories,Center,Cover,PlayersR,BagR,FactoriesR,CenterR,CoverR,0).
+
+%round/11
+%round(Players,Bag,Factories,Center,Cover,PlayersR,BagR,FactoriesR,CenterR,CoverR,IsOverRonda)
+%This is where the players decide their strategies and follow them, until the end of the round, without passing the tiles of the pattern lines to the wall, until the factories and center are empty.
+round(Players,Bag,Factories,Center,Cover,Players,Bag,Factories,Center,Cover,1):!.
+round([Player|PlayersList],Bag,Factories,Center,Cover,PlayersListR,BagR,FactoriesR,CenterR,CoverR,0):-playPlayer(Player,Bag,Factories,Center,Cover,PlayerT,BagT,FactoriesT,CenterT,CoverT),append(PlayersList,[PlayerT],PlayersListT),terminoRonda(FactoriesT,IsOverRonda),round(PlayersListT,BagT,FactoriesT,CenterT,CoverT,PlayersListR,BagR,FactoriesR,CenterR,CoverR,IsOverRonda).
+
+%playPlayer/10
+%playPlayer(Player,Bag,Factories,Center,Cover,PlayerR,BagR,FactoriesR,CenterR,CoverR)
+%This is where a Player selects tiles and puts them on his board.
+playPlayer(Player,Bag,Factories,Center,Cover,PlayerR,BagR,FactoriesR,CenterR,CoverR):-
+selectStrategy(Player,Factories,Center,FilaPatronT,CogiCenterBoolT,ColorT,FactorySelecT),putStrategyBoard(Factories,Center,Bag,Cover,Player,FilaPatronT,CogiCenterBoolT,FactorySelecT,ColorT,FactoriesR,CenterR,BagR,CoverR,PlayerR).
