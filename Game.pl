9c10b6c1ef1cb100a162e1525f08ddce9b5b5f85
dayfundora <dayfundoraglez@gmail.com>
2021-04-16 01:31:23 -0400

    Add play to Game

--- Game.pl
+++ Game.pl
@@ -12,10 +12,20 @@
 %prepareGame(cantPlayers,Players,Bag,Factories,Center,Cover)
 %Is in charge of initializing the board (Players, Bag, Factories, Center)
 prepareGame(cantPlayers,Players,Bag,Factories,Center,Cover):-
-print('  Initializing '),
-initializePlayers(cantPlayers,Players),
-initializeBag(BagT),
-initializeFactory(cantPlayers,BagT,Factories,BagR),
-print('  Factories Full'),
-initializeCover(Cover),
-initializeCenter(Center).
+ print('  Initializing '),
+ initializePlayers(cantPlayers,Players),
+ initializeBag(BagT),
+ initializeFactory(cantPlayers,BagT,Factories,BagR),
+ print('  Factories Full'),
+ initializeCover(Cover),
+ initializeCenter(Center).
+
+%play/6
+%play(Players,Bag,Factories,Center,Cover,IsOver,PlayersR)
+%This is where the game happens, each player plays, the rounds are repeated, until someone lines up.
+play(Players,Bag,Factories,Center,Cover,1,Players):-print('  Game Over '),!.
+play(Players,Bag,Factories,Center,Cover,0,PlayersR):-offertFactory(Players,Bag,Factories,Center,Cover,PlayersT,BagT,FactoriesT,CenterT,CoverT),
+ tilingWalls(PlayersT,CoverT,PlayersS,CoverS), 
+ prepareNextRound(PlayersS,BagT,FactoriesT,CenterT,CoverS,PlayersM,BagR,FactoriesR,CenterR,CoverR), 
+ print('  The round is over ')
+ play(PlayersM,BagR,FactoriesR,CenterR,CoverR,IsOver,PlayersR).
